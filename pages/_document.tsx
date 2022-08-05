6a687497d84caf2131becff8c33be8e761519ab6
Garret Harp <garretcharp@gmail.com>
2022-08-04 20:02:21 -0500

    init site

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,23 @@
-import { Head, Html, Main, NextScript } from 'next/document'
-import { getCssText } from '../stitches.config'
+import { Head, Html, Main, NextScript } from "next/document";
+import { getCssText, styled } from "../stitches.config";
+
+const Body = styled("body", {
+	backgroundColor: "hsl(200 7.0% 8.8%)",
+});
 
 export default function MyDocument() {
-  return (
-    <Html lang="en">
-      <Head>
-        <style
-          id="stitches"
-          dangerouslySetInnerHTML={{ __html: getCssText() }}
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<style
+					id="stitches"
+					dangerouslySetInnerHTML={{ __html: getCssText() }}
+				/>
+			</Head>
+			<Body>
+				<Main />
+				<NextScript />
+			</Body>
+		</Html>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,44 @@
-import Head from 'next/head'
-import StitchesLogo from '../components/StitchesLogo'
-import { styled } from '../stitches.config'
+import Head from "next/head";
+import React from "react";
+import { styled } from "../stitches.config";
 
-const Box = styled('div', {})
+const Text = styled("p", {
+	fontFamily: "$system",
+	color: "white",
+});
 
-const Text = styled('p', {
-  fontFamily: '$system',
-  color: '$hiContrast',
-})
+const Container = styled("div", {
+	marginX: "auto",
+	paddingX: "$3",
 
-const Link = styled('a', {
-  fontFamily: '$system',
-  textDecoration: 'none',
-  color: '$purple600',
-})
-
-const Container = styled('div', {
-  marginX: 'auto',
-  paddingX: '$3',
-
-  variants: {
-    size: {
-      1: {
-        maxWidth: '300px',
-      },
-      2: {
-        maxWidth: '585px',
-      },
-      3: {
-        maxWidth: '865px',
-      },
-    },
-  },
-})
+	variants: {
+		size: {
+			1: {
+				maxWidth: "300px",
+			},
+			2: {
+				maxWidth: "585px",
+			},
+			3: {
+				maxWidth: "865px",
+			},
+		},
+	},
+});
 
 export default function Home() {
-  return (
-    <Box css={{ paddingY: '$6' }}>
-      <Head>
-        <title>Use Stitches with Next.js</title>
-      </Head>
-      <Container size={{ '@initial': '1', '@bp1': '2' }}>
-        <StitchesLogo />
-        <Text as="h1">Hello, from Stitches.</Text>
-        <Text>
-          For full documentation, visit{' '}
-          <Link href="https://stitches.dev">stitches.dev</Link>.
-        </Text>
-      </Container>
-    </Box>
-  )
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Garret Harp | Software Engineer</title>
+			</Head>
+			<Container size={{ "@initial": "1", "@bp1": "2" }}>
+				<Text as="h1">Hello, my name is Garret.</Text>
+				<Text>
+					I have a passion for web development, and solving problems. I enjoy
+					creating things that benefit others.
+				</Text>
+			</Container>
+		</React.Fragment>
+	);
 }
